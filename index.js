78d3093f0d17aad8b3457001905cc1f923a416d7
Doguhan Uluca <duluca@gmail.com>
2016-03-18 21:14:54 -0400

    Added node server

---
+++ index.js
@@ -0,0 +1,28 @@
+/**
+ * Created by doguhanuluca on 3/18/16.
+ */
+'use strict';
+
+var http = require('http');
+var express = require('express');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+var compression = require('compression');
+var path = require('path');
+
+var app = express();
+
+app.use(compression());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(logger('dev'));
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+var server = http.createServer(app);
+
+var port = process.env.PORT || 3000
+
+server.listen(port, function () {
+  console.log('Server listening on port ' + port);
+});
